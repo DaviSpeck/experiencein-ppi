790cfb5652f78fcd011462456a713e1b514ae1f1
DaviSpeck <davi.speck86@gmail.com>
2022-04-26 17:43:06 -0300

    Finalizando Módulo 4

--- experiencein/perfis/views.py
+++ experiencein/perfis/views.py
@@ -6,11 +6,5 @@ def index(request):
 
 def exibir(request, perfil_id):
 
-    perfil = Perfil()
-
-    if perfil_id == 1:
-        perfil = Perfil('Fábio Henrique', 'fabio.oliveira@ifb.edu.br', '222222', 'IFB')
-    if perfil_id == 2:
-        perfil = Perfil('Elon Musk', 'elon.musk@tesla.com', '333333', 'Tesla') 
-
-    return render(request, 'perfil.html', {'perfil' : perfil})
+    perfil = Perfil.objects.get(id=perfil_id)
+    return render(request, 'perfil.html', { "perfil" : perfil})
